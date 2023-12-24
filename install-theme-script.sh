7b4673934a8578534e731d01d04c54ee29bd540b
iWisp360 <114373500+iWisp360@users.noreply.github.com>
2023-12-24 18:18:03 -0500

    Update install-theme-script.sh

--- install-theme-script.sh
+++ install-theme-script.sh
@@ -1,73 +1,63 @@
 #!/bin/sh
-echo "
-Verifying root permsisions...
+echo "Verifying root permsisions...
 "
 
 if [ "$(id -u)" -ne 0 ]; then
 
-echo "
-Superuser permissions verification failed, please execute this script with root permissions.
+echo "Superuser permissions verification failed, please execute this script with root permissions.
 Halting script...
 "
   exit 1
 
 fi
 
-echo "
-Verifying the existence of plymouth...
+echo "Verifying the existence of plymouth...
 "
 
 if pacman -Qi plymouth &> /dev/null; then
 
-  echo "
-Plymouth found!
+  echo "Plymouth found!
 "
 
 else
-  echo "
-Plymouth is not installed, Here you can install it: https://wiki.archlinux.org/title/Plymouth 
+  echo "Plymouth is not installed, Here you can install it: https://wiki.archlinux.org/title/Plymouth 
 Halting script...
 "
  exit 1
 fi
 
-echo "
-Verifying the existence of the compressed tar file included in the download of the theme...
+echo "Verifying the existence of the compressed tar file included in the download of the theme...
 "
 
-theme-file="./spinner-endeavouros.tar"
+file_directory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+compressed_file="spinner-endeavouros.tar"
 
-if [ -e "$theme-file" ]; then
+if [ -e "$file_directory/$compressed_file" ]; then
 
-echo "
-Theme compressed file in place! 
+echo "Theme compressed file in place! 
 
 Verification steps finished, proceeding to the installation...
 "
 
 else
 
-echo "
-The compressed theme file (spinner-endeavouros.tar) is misplaced or it doesn't exist... Please verify the existence of the compressed file and place it in the same directory of this script...
+echo "The compressed theme file (spinner-endeavouros.tar) is misplaced or it doesn't exist... Please verify the existence of the compressed file and place it in the same directory of this script...
 "
 exit 1
 
 fi
 
-echo "
-Extracting theme into Plymouth themes...
+echo "Extracting theme into Plymouth themes...
 "
 
 tar --extract -f spinner-endeavouros.tar --directory=/usr/share/plymouth/themes/
 
-echo "
-Changing the default theme
+echo "Changing the default theme...
 "
 
 plymouth-set-default-theme spinner-endeavouros
 
-echo "
-Regenerating initramfs...
+echo "Regenerating initramfs...
 "
 
 mkinitcpio="/bin/mkinitcpio"
@@ -75,32 +65,27 @@ dracut="/bin/dracut"
 
 if [ -e "$mkinitcpio" ]; then
 
-echo "
-Regenerating initramfs with mkinitcpio...
+echo "Using mkinitcpio...
 "
 
 mkinitcpio -P
 
-echo "
-Done! You have to reboot to apply the theme.
+echo "Done! You have to reboot to apply the theme.
 "
 
 elif [ -e "$dracut" ]; then
 
-echo "
-Regenerating initramfs with dracut...
+echo "Using dracut...
 "
 
 dracut -f
 
-echo "
-Done! You have to reboot to apply the theme.
+echo "Done! You have to reboot to apply the theme.
 "
 
 else
 
-echo "
-Done! But you don't have either mkinitcpio or dracut, please, regenerate your initramfs manually to apply the theme in early boot.
+echo "Done! But you don't have either mkinitcpio or dracut, please, regenerate your initramfs manually to apply the theme in early boot.
 "
 
 fi
